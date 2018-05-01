6f917183a5b2993b2f0116d0e2885a91fdde0ea7
Alex Routledge <alex.routledge@veritas.com>
2018-05-01 17:41:34 +0100

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="app">
+      <dynamic-form 
+        [config]="config"
+        (submitted)="formSubmitted($event)">
+      </dynamic-form>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  config = [
+    {
+      type: 'input',
+      label: 'Full name',
+      name: 'name',
+      placeholder: 'Enter your name',
+    },
+    {
+      type: 'select',
+      label: 'Favourite food',
+      name: 'food',
+      options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+      placeholder: 'Select an option',
+    },
+    {
+      label: 'Submit',
+      name: 'submit',
+      type: 'button',
+    },
+  ];
+  formSubmitted(value) {
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DynamicFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
